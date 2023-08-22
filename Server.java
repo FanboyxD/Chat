734ed10c2b38e4404fcfadb6a9f3160403a03d88
FanboyxD <48070782+FanboyxD@users.noreply.github.com>
2023-08-22 12:42:42 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,83 +1,67 @@
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Server {
-    public void startServer() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(12345); // Port for clients to connect
-            System.out.println("El servidor del chat esta en el puerto 12345");
+public class ChatServer {
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                int clientPort = findAvailablePort(); // Find an available port for the client
-
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                out.println(clientPort); // Send the assigned port to the client
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private static final int STARTING_PORT = 12345;
+    private static int nextPort = STARTING_PORT;
+    private static List<PrintWriter> clientWriters = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(0); // Let the system choose an available port
-            int port = serverSocket.getLocalPort();
-            System.out.println("El servidor del chat esta en el puerto " + port);
+            ServerSocket serverSocket = new ServerSocket(STARTING_PORT);
+            System.out.println("Servidor en ejecución. Esperando conexiones...");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                new Thread(new ClientHandler(clientSocket)).start();
+                System.out.println("Cliente conectado desde: " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+
+                int clientPort = nextPort;
+                nextPort++;
+
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+                clientWriters.add(writer);
+
+                Thread clientThread = new Thread(new ClientHandler(clientSocket, writer, clientPort));
+                clientThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private static int findAvailablePort() {
-        for (int port = 1024; port <= 65535; port++) {
-            try {
-                ServerSocket socket = new ServerSocket(port);
-                socket.close();
-                return port;
-            } catch (IOException e) {
-                // Port is already in use, try the next one
-            }
+
+    private static class ClientHandler implements Runnable {
+
+        private Socket clientSocket;
+        private PrintWriter writer;
+        private int clientPort;
+
+        public ClientHandler(Socket clientSocket, PrintWriter writer, int clientPort) {
+            this.clientSocket = clientSocket;
+            this.writer = writer;
+            this.clientPort = clientPort;
         }
-        throw new RuntimeException("No hay puertos disponibles");
-    }
-    @Override
-    public String toString() {
-        return "Servidor []";
-    }
-}
-class ClientHandler implements Runnable {
-    private Socket clientSocket;
-    private BufferedReader in;
-    private PrintWriter out;
 
-    public ClientHandler(Socket socket) {
-        this.clientSocket = socket;
-    }
-
-    @Override
-    public void run() {
-        try {
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            String message;
-            while ((message = in.readLine()) != null) {
-                System.out.println("Mensaje recibido: " + message);
-                // You can implement message handling and forwarding here
-
-                // For this basic example, let's just echo the message back
-                out.println("Servidor: " + message);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
+        @Override
+        public void run() {
             try {
-                clientSocket.close();
+                Scanner scanner = new Scanner(clientSocket.getInputStream());
+
+                while (true) {
+                    String message = scanner.nextLine();
+                    System.out.println("Mensaje recibido del puerto " + clientPort + ": " + message);
+
+                    for (PrintWriter clientWriter : clientWriters) {
+                        if (clientWriter != writer) {
+                            clientWriter.println("Desde el puerto " + clientPort + ": " + message);
+                        }
+                    }
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
