2c4bfd696c4c8147f461fb2940c73d99c534230d
FanboyxD <48070782+FanboyxD@users.noreply.github.com>
2023-08-22 12:41:47 -0600

    Update ChatApp.java

--- ChatApp.java
+++ ChatApp.java
@@ -1,79 +1,62 @@
-import java.io.PrintWriter;
-
+// ChatApp.java
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ChatApp extends Application {
-    private Cliente client;
-    private PrintWriter serverOut;
-    @Override
-    public void start(Stage primaryStage) {
-        Thread serverThread = new Thread(() -> {
-            Server server = new Server();
-            server.startServer(); // Implement this method in your ChatServer class
-        });
-        serverThread.start();
-        // Layout for the main chat window
-        BorderPane mainLayout = new BorderPane();
 
-        // Text area to display chat messages
-        TextArea chatArea = new TextArea();
-        chatArea.setEditable(false);
-        mainLayout.setCenter(chatArea);
+    private ChatClient client;
+    private TextArea chatArea;
+    private TextField messageField;
+    private Label portLabel;
 
-        // HBox for entering messages and sending
-        TextField messageField = new TextField();
-        Button sendButton = new Button("Send");
-        sendButton.setOnAction(event -> {
-            String message = messageField.getText(); // Get the message from the input field
-    
-    // Send the message to the server through the client
-    // Implement this logic in your ChatClient class
-            messageField.clear(); // Clear the input field after sending
-            chatArea.appendText("Yo: " + message + "\n");
-        });
-        HBox inputBox = new HBox(messageField, sendButton);
-        inputBox.setSpacing(10);
-        inputBox.setPadding(new Insets(10));
-        mainLayout.setBottom(inputBox);
-
-        // VBox for entering server IP and port
-        TextField ipField = new TextField();
-        TextField portField = new TextField();
-        Label ipLabel = new Label("IP del servidor:");
-        Label portLabel = new Label("Puerto disponible:");
-        VBox serverInfoBox = new VBox(ipLabel, ipField, portLabel, portField);
-        serverInfoBox.setSpacing(10);
-        serverInfoBox.setPadding(new Insets(10));
-        mainLayout.setLeft(serverInfoBox);
-
-        // Create the Connect button
-        Button connectButton = new Button("Conectar");
-        connectButton.setOnAction(event -> {
-            String serverIP = ipField.getText();
-            int serverPort = Integer.parseInt(portField.getText());
-
-            // Start the client in a separate thread
-            Thread clientThread = new Thread(() -> {
-                Cliente client = new Cliente(serverIP, serverPort);
-                client.startClient(); // Implement this method in your ChatClient class
-                
-            });
-            clientThread.start();
-        });
-        serverInfoBox.getChildren().add(connectButton);
-
-        // Create the scene and set it to the stage
-        Scene scene = new Scene(mainLayout, 400, 300);
-        primaryStage.setTitle("Aplicacion de Chat");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Chat App");
+
+        // Crear componentes de la interfaz gráfica
+        chatArea = new TextArea();
+        messageField = new TextField();
+        Button sendButton = new Button("Enviar");
+        portLabel = new Label("Puerto: -");
+
+        // Configurar acción del botón Enviar
+        sendButton.setOnAction(event -> sendMessage());
+
+        // Crear contenedor principal y agregar componentes
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(10));
+        root.getChildren().addAll(chatArea, messageField, sendButton, portLabel);
+
+        // Crear escena y mostrar ventana
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        // Conectar al servidor
+        client = new ChatClient();
+        String serverIp = "127.0.0.1"; // Cambiar por la dirección IP del servidor
+        int port = 12345; // Cambiar por el puerto del servidor
+        client.connect(serverIp, port, chatArea);
+
+        // Mostrar el puerto en la interfaz gráfica
+        portLabel.setText("Puerto: " + client.getPort());
+    }
+
+    private void sendMessage() {
+        String message = messageField.getText();
+        client.sendMessage(message);
+        chatArea.appendText("Enviado: " + message + "\n");
+        messageField.clear();
     }
 }
